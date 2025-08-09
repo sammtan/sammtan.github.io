f925accaa704ac7de8e5ee284e0e822e78eb2dfe
sammtan <sammtan.rt@gmail.com>
2025-08-09 23:29:23 +0700

    force-static fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://sammtan.github.io'
   const currentDate = new Date().toISOString().split('T')[0]
